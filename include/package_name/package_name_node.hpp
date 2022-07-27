55ee49cdcbb3f02e09a381fd05145c733888a406
scepter914 <scepter914@gmail.com>
2022-07-27 19:34:40 +0900

    update LICENSE
    Signed-off-by: scepter914 <scepter914@gmail.com>

--- include/package_name/package_name_node.hpp
+++ include/package_name/package_name_node.hpp
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
-// Copyright 2021 Kenji Miyake
+// Copyright 2022 Satoshi Tanaka
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
