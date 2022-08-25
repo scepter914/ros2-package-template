07ccde5ec0d7242d530e66d11a1cc91a799f4860
scepter914 <scepter914@gmail.com>
2022-08-25 15:55:39 +0900

    update fish script
    Signed-off-by: scepter914 <scepter914@gmail.com>

--- setup.fish
+++ setup.fish
@@ -8,13 +8,13 @@ argparse $options -- $argv
 set -l snake_case_package_name (basename $script_dir)
 set -l PascalCasePackageName (echo "$snake_case_package_name" | sed -E 's/^(.)/\U\1/g' | sed -E 's/_(.)/\U\1/g')
 set -l UPPER_SNAKE_CASE_PACKAGE_NAME (basename $script_dir | sed -E 's/(.*)/\U\1/g')
-set -l "Satoshi Tanaka" "TIER IV, Inc."
 
 cd $script_dir
 sd "GIT_CONFIG_EMAIL@example.com" (git config --global user.email) $script_dir/package.xml
 sd "GIT_CONFIG_EMAIL@example.com" (git config --global user.email) $script_dir/package_autoware.xml
 sd "GIT_CONFIG_NAME" (git config --global user.name) $script_dir/package.xml
 sd "GIT_CONFIG_NAME" (git config --global user.name) $script_dir/package_autoware.xml
+sd "Satoshi Tanaka" "TIER IV, Inc."
 
 set -l target_files (fd -t f --exclude "*.fish" --exclude "README.md")
 sd "package_name" $snake_case_package_name $target_files
