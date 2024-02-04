5764acfd1301a72636744759424c0674adb3838f
scepter914 <scepter914@gmail.com>
2024-02-04 22:52:50 +0900

    update template
    Signed-off-by: scepter914 <scepter914@gmail.com>

--- src/package_name_node/package_name.cpp
+++ src/package_name_node/package_name.cpp
@@ -1,4 +1,4 @@
-// Copyright 2022 Satoshi Tanaka
+// Copyright 2024 Satoshi Tanaka
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
